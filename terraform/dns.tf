fd521e0dff9b5606d81b4ae80d7ea2247b78c67d
Brad Murray <bradtgmurray@gmail.com>
2026-02-08 18:20:35 -0500

    Add Cloudflare DNS record for survivor subdomain

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -7,3 +7,13 @@ resource "cloudflare_record" "bestmount" {
   proxied = false # Must be false for Vercel SSL to work
   ttl     = 1     # Auto TTL when not proxied
 }
+
+# survivor.bradtgmurray.com -> Vercel
+resource "cloudflare_record" "survivor" {
+  zone_id = var.bradtgmurray_zone_id
+  name    = "survivor"
+  content = "cname.vercel-dns.com"
+  type    = "CNAME"
+  proxied = false # Must be false for Vercel SSL to work
+  ttl     = 1     # Auto TTL when not proxied
+}
